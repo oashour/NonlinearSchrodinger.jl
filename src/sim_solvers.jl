4ad9094d6817d7334400a2fb88b3738a68c5044d
Omar Ashour <omar.ashour22@gmail.com>
2020-10-16 07:37:36 -0700

    Improve pruning and use views in solver

--- src/sim_solvers.jl
+++ src/sim_solvers.jl
@@ -2,6 +2,11 @@ export solve!
 
 function solve!(sim::Simulation)
     sim.ψ[1, :] = sim.ψ₀
+    if sim.αₚ > 0 
+        ind_p = [i for i in 2:(sim.box.Nₜ÷2+1) if (i-1)%sim.box.n_periods != 0]
+        ind_p = sort([ind_p; sim.box.Nₜ.-ind_p.+2])
+        #println(ind_p)
+    end
 
     println("==========================================")
     println("Solving cubic NLSE with the following options:")
@@ -9,23 +14,27 @@ function solve!(sim::Simulation)
     @showprogress 1 "Computing..." for i = 1:sim.box.Nₓ-1
     #for i = 1:sim.box.Nₓ-1
         if sim.algorithm == "2S"
-            sim.ψ[i+1, :] = T2(sim.ψ[i, :], sim.box.ω, sim.box.dx)
+            @views sim.ψ[i+1, :] = T2(sim.ψ[i, :], sim.box.ω, sim.box.dx)
         elseif sim.algorithm == "4S"
-            sim.ψ[i+1, :] = T4S(sim.ψ[i, :], sim.box.ω, sim.box.dx)
+            @views sim.ψ[i+1, :] = T4S(sim.ψ[i, :], sim.box.ω, sim.box.dx)
         elseif sim.algorithm == "6S"
-            sim.ψ[i+1, :] = T6S(sim.ψ[i, :], sim.box.ω, sim.box.dx)
+            @views sim.ψ[i+1, :] = T6S(sim.ψ[i, :], sim.box.ω, sim.box.dx)
         elseif sim.algorithm == "8S"
-            sim.ψ[i+1, :] = T8S(sim.ψ[i, :], sim.box.ω, sim.box.dx)
+            @views sim.ψ[i+1, :] = T8S(sim.ψ[i, :], sim.box.ω, sim.box.dx)
         else
             throw(ArgumentError("Algorithm type unknown, please check the documentation"))
         end
         # Pruning
+        # TODO: get rid of this extra ψ and rewrite more elegantly
         if sim.αₚ > 0 
-            sim.ψ[i+1, :] = prune(sim.ψ[i+1, :], sim)
+            ψ = sim.ψ[i+1, :]
+            fft!(ψ)
+            @views ψ[ind_p] .*= exp.(-sim.αₚ*abs.(ψ[ind_p]))
+            ifft!(ψ)
+            sim.ψ[i+1, :] = ψ
         end
     end #for
     sim.solved = true
-    
 
     println("Computation Done!")
     println("==========================================")
@@ -33,49 +42,18 @@ function solve!(sim::Simulation)
     return nothing
 end #solve
 
-function prune(ψ, sim)
-    #(naive approach)
-    fft!(ψ)
-    for i = 2:Int(sim.box.Nₜ/2)+1
-        if (i - 1) % sim.box.n_periods != 0
-           ψ[i] *= exp(-sim.αₚ*abs(ψ[i])) # + component
-            ψ[sim.box.Nₜ - i + 2] *= exp(-sim.αₚ*abs(ψ[sim.box.Nₜ - i + 2])) # - component
-        end
-    end 
-    ifft!(ψ)
-
-
-    return ψ
-
-    #   ind = []
-    #   for n = 1:Int(ceil(22/2/3))
-    #   for m = 1:3-1
-    #   index = 3*n-m
-    #   if index <  22/2
-    #      append!(ind, index)
-    #      append!(ind, -index)
-    #   elseif index == 22/2
-    #      append!(ind, -index)
-    #   end
-    #   end
-    #   end
-    #   println(sort(ind))
-end
-
-
 function T2(ψ, ω, dx)
     # Nonlinear
-    V = -1 * abs.(ψ) .^ 2.0                      
-    ψ = exp.(-im * dx / 2.0 * V) .* ψ            
+    V = -1*abs.(ψ).^2                      
+    ψ .*= exp.(-im * dx/2 * (-1*abs.(ψ).^2)) 
 
     # Kinetic
     fft!(ψ)                                   
-    ψ = ifftshift(exp.(-im * dx * ω .^ 2 / 2)) .* ψ  
+    ψ .*= ifftshift(exp.(-im * dx * ω .^ 2 / 2)) 
     ifft!(ψ)                                  
 
     # Nonlinear
-    V = -1 * abs.(ψ) .^ 2                   
-    ψ = exp.(-im * dx / 2 * V) .* ψ   
+    ψ .*= exp.(-im * dx/2 * (-1*abs.(ψ).^2)) 
 
     return ψ
 end #T2
@@ -94,7 +72,7 @@ function T4S(ψ, ω, dx)
     return ψ
 end # T4S
 
-function T6S(ψ, ω, dx; pruning = "off", n_periods = 1)
+function T6S(ψ, ω, dx)
     s = 2^(1 / 5)
     os = 1 / (2 - s)
 
@@ -108,7 +86,7 @@ function T6S(ψ, ω, dx; pruning = "off", n_periods = 1)
     return ψ
 end #T6S
 
-function T8S(ψ, ω, dx; pruning = "off", n_periods = 1)
+function T8S(ψ, ω, dx)
     s = 2^(1 / 7)
     os = 1 / (2 - s)
 
