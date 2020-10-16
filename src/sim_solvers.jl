0398d9ad93442a436d59492a920dae1c19de872a
Omar Ashour <omar.ashour22@gmail.com>
2020-10-16 09:00:52 -0700

    Use FFT Plans without views

--- src/sim_solvers.jl
+++ src/sim_solvers.jl
@@ -1,26 +1,33 @@
 export solve!
 
 function solve!(sim::Simulation)
+    println("==========================================")
+    println("Solving cubic NLSE with the following options:")
+    # Copy in x = 0 array
     sim.ψ[1, :] = sim.ψ₀
+    # Check for pruning and calculate indices
     if sim.αₚ > 0 
         ind_p = [i for i in 2:(sim.box.Nₜ÷2+1) if (i-1)%sim.box.n_periods != 0]
         ind_p = sort([ind_p; sim.box.Nₜ.-ind_p.+2])
-        #println(ind_p)
     end
+    # Generate FFT Plans to optimize performance
+    println("Generating FFT Plan")
+    F = plan_fft!(sim.ψ[1, :]) # Plan
+    F̃ = inv(F) # Inverse plan, now cached
 
-    println("==========================================")
-    println("Solving cubic NLSE with the following options:")
+    # Print info about simulation
     print(sim)
+
+    # Start loop and pick algorithm
     @showprogress 1 "Computing..." for i = 1:sim.box.Nₓ-1
-    #for i = 1:sim.box.Nₓ-1
         if sim.algorithm == "2S"
-            @views sim.ψ[i+1, :] = T2(sim.ψ[i, :], sim.box.ω, sim.box.dx)
+            sim.ψ[i+1, :] = T2(sim.ψ[i, :], sim.box.ω, sim.box.dx, F)
         elseif sim.algorithm == "4S"
-            @views sim.ψ[i+1, :] = T4S(sim.ψ[i, :], sim.box.ω, sim.box.dx)
+            sim.ψ[i+1, :] = T4S(sim.ψ[i, :], sim.box.ω, sim.box.dx, F)
         elseif sim.algorithm == "6S"
-            @views sim.ψ[i+1, :] = T6S(sim.ψ[i, :], sim.box.ω, sim.box.dx)
+            sim.ψ[i+1, :] = T6S(sim.ψ[i, :], sim.box.ω, sim.box.dx, F)
         elseif sim.algorithm == "8S"
-            @views sim.ψ[i+1, :] = T8S(sim.ψ[i, :], sim.box.ω, sim.box.dx)
+            sim.ψ[i+1, :] = T8S(sim.ψ[i, :], sim.box.ω, sim.box.dx, F)
         else
             throw(ArgumentError("Algorithm type unknown, please check the documentation"))
         end
@@ -28,9 +35,9 @@ function solve!(sim::Simulation)
         # TODO: get rid of this extra ψ and rewrite more elegantly
         if sim.αₚ > 0 
             ψ = sim.ψ[i+1, :]
-            fft!(ψ)
-            @views ψ[ind_p] .*= exp.(-sim.αₚ*abs.(ψ[ind_p]))
-            ifft!(ψ)
+            F*ψ
+            @views ψ[ind_p] .*= exp.(-sim.αₚ*abs.(ψ[i+1, ind_p]))
+            inv(F)*ψ
             sim.ψ[i+1, :] = ψ
         end
     end #for
@@ -42,15 +49,15 @@ function solve!(sim::Simulation)
     return nothing
 end #solve
 
-function T2(ψ, ω, dx)
+function T2(ψ, ω, dx, F)
     # Nonlinear
     V = -1*abs.(ψ).^2                      
     ψ .*= exp.(-im * dx/2 * (-1*abs.(ψ).^2)) 
 
     # Kinetic
-    fft!(ψ)                                   
+    F*ψ
     ψ .*= ifftshift(exp.(-im * dx * ω .^ 2 / 2)) 
-    ifft!(ψ)                                  
+    inv(F)*ψ
 
     # Nonlinear
     ψ .*= exp.(-im * dx/2 * (-1*abs.(ψ).^2)) 
@@ -58,44 +65,44 @@ function T2(ψ, ω, dx)
     return ψ
 end #T2
 
-function T4S(ψ, ω, dx)
+function T4S(ψ, ω, dx, F)
     s = 2^(1 / 3)
     os = 1 / (2 - s)
 
     ft = os
     bt = -s * os
 
-    ψ = T2(ψ, ω, ft * dx)
-    ψ = T2(ψ, ω, bt * dx)
-    ψ = T2(ψ, ω, ft * dx)
+    ψ = T2(ψ, ω, ft * dx, F)
+    ψ = T2(ψ, ω, bt * dx, F)
+    ψ = T2(ψ, ω, ft * dx, F)
 
     return ψ
 end # T4S
 
-function T6S(ψ, ω, dx)
+function T6S(ψ, ω, dx, F)
     s = 2^(1 / 5)
     os = 1 / (2 - s)
 
     ft = os
     bt = -s * os
 
-    ψ = T4S(ψ, ω, ft * dx)
-    ψ = T4S(ψ, ω, bt * dx)
-    ψ = T4S(ψ, ω, ft * dx)
+    ψ = T4S(ψ, ω, ft * dx, F)
+    ψ = T4S(ψ, ω, bt * dx, F)
+    ψ = T4S(ψ, ω, ft * dx, F)
 
     return ψ
 end #T6S
 
-function T8S(ψ, ω, dx)
+function T8S(ψ, ω, dx, F)
     s = 2^(1 / 7)
     os = 1 / (2 - s)
 
     ft = os
     bt = -s * os
 
-    ψ = T6S(ψ, ω, ft * dx)
-    ψ = T6S(ψ, ω, bt * dx)
-    ψ = T6S(ψ, ω, ft * dx)
+    ψ = T6S(ψ, ω, ft * dx, F)
+    ψ = T6S(ψ, ω, bt * dx, F)
+    ψ = T6S(ψ, ω, ft * dx, F)
 
     return ψ
 end #T8S
