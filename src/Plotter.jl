2b33f520c0aab5a78e829790c1533f4e2f7917c5
Omar Ashour <omar.ashour22@gmail.com>
2020-10-26 17:59:41 -0700

    Add contourf plotting options

--- src/Plotter.jl
+++ src/Plotter.jl
@@ -1,6 +1,7 @@
 module Plotter
 using ..Simulation
-using Plots; gr() 
+using Plots; gr()
+using Plots.PlotMeasures
 using LaTeXStrings
 using FFTW
 
@@ -70,13 +71,92 @@ function plot_ψ(sim; mode = "density", power=1, x_res=500, t_res=512)
 
     # Plot
     if mode == "density"
-        p = heatmap(t, x, abs.(ψ').^power, tick_direction=:out, colorbar_title=clabel)
+        p = heatmap(t, x, abs.(ψ').^power, 
+                    tick_direction=:out, 
+                    colorbar_title=clabel,
+                    margin = 4mm,
+                    xlabel=L"t",
+                    ylabel=L"x")
     elseif mode == "surface"
-        p = surface(t, x, abs.(ψ').^power, zlabel=clabel, camera=(35, 68))
+        p = surface(t, x, abs.(ψ').^power, 
+                    colorbar_title=clabel, 
+                    camera=(20, 68),
+                    grid=false,
+                    xlabel=L"t",
+                    ylabel=L"x")
+    elseif mode == "contourf"
+        # Should also adjust colorbar ticks when the feature is added
+        p = contourf(t, x, abs.(ψ').^power, 
+                    tick_direction=:out, 
+                    colorbar_title=clabel,
+                    margin = 4mm,
+                    xlabel=L"t",
+                    ylabel=L"x",
+                    levels=[0.25,0.4,0.8,1,1.25,1.5,1.75],
+                    clims=(0.25,1.75))
+    elseif mode == "contourf_clean"
+        p = contourf(t, x, abs.(ψ').^power, 
+                    showaxis=false,
+                    ticks=[],
+                    levels=[0.2,0.4,0.8,1,1.25,1.5,1.8],
+                    colorbar=false)
+    elseif mode == "contourf_inset"
+        l = @layout [
+            a 
+            d{0.793h}
+        ]
+        ps = surface(t, x, abs.(ψ').^power, 
+                    colorbar_title=clabel,
+                    zlabel=clabel,
+                    camera=(20, 68),
+                    grid=false,
+                    xlabel=L"t",
+                    ylabel=L"x")
+        pe = contourf(0,
+                    showaxis=false,
+                    colorbar=false,
+                    ticks=[],
+                    grid=false)
+        p = plot(pe, ps, layout = l, size=(600, 500), margins=1mm)
+        pc = contourf!(t, x, abs.(ψ').^power, 
+                    inset = (2, bbox(-0.09, -0.39, 0.55, 0.55)), 
+                    subplot=3,
+                    showaxis=false,
+                    ticks=[],
+                    levels=[0.2,0.4,0.8,1,1.25,1.5,1.8],
+                    colorbar=false)
+
+    elseif mode == "contourf_top"
+        l = @layout [
+             [a{0.01w} b{0.89w} c{0.1w}]
+            d{0.6666667h}
+        ]
+        ps = surface(t, x, abs.(ψ').^power, 
+                    colorbar_title=clabel,
+                    zlabel=clabel,
+                    camera=(20, 68),
+                    grid=false,
+                    xlabel=L"t",
+                    ylabel=L"x")
+        pe1 = contourf(0,
+                    showaxis=false,
+                    colorbar=false,
+                    ticks=[],
+                    grid=false)
+        pc = contourf(t, x, abs.(ψ').^power, 
+                    showaxis=false,
+                    ticks=[],
+                    levels=[0.2,0.4,0.8,1,1.25,1.5,1.8],
+                    colorbar=false)
+        pe2 = contourf(0,
+                    showaxis=false,
+                    colorbar=false,
+                    ticks=[],
+                    grid=false)
+
+        p = plot(pe1, pc, pe2, ps, layout = l, size=(600, 600))
     end
     # Adjust Attributes
-    xlabel!(L"x")
-    ylabel!(L"t")
     xlims!((-sim.T/2*sim.box.n_periods, sim.T/2*sim.box.n_periods))
     ylims!((minimum(sim.box.x), maximum(sim.box.x)))
 
@@ -105,6 +185,8 @@ function plot_ψ̃(sim; mode = "density", x_res=500, ω_res=512, skip = 1, n_lin
     # Compute sampling interval
     xₛ = Int(ceil(sim.box.Nₓ/x_res))
     ωₛ = Int(ceil(sim.box.Nₜ/ω_res))
+    n = Int.(round.(sim.box.ω/sim.Ω)) 
+    n = n[1:ωₛ:end]
     ω = sim.box.ω[1:ωₛ:end]
     x = sim.box.x[1:xₛ:end]
     # Plot
@@ -112,9 +194,13 @@ function plot_ψ̃(sim; mode = "density", x_res=500, ω_res=512, skip = 1, n_lin
         # Downsample
         ψ̃ = sim.ψ̃[1:ωₛ:end, 1:xₛ:end]
 
-        p = heatmap(ω, x, log.(abs.(ψ̃')), tick_direction=:out, colorbar_title=L"\log|\tilde{\psi}|")
-        xlabel!(L"\omega")
-        ylabel!(L"x")
+        #heatmap(n, x, log.(abs.(ψ̃')), xmirror = true)
+        p = heatmap(ω, x, log.(abs.(ψ̃')), 
+                    tick_direction=:out, 
+                    colorbar_title=L"\log|\tilde{\psi}|",
+                    xlabel = L"\omega",
+                    ylabel = L"x",
+                    margin = 4mm)
         xlims!((-sim.box.Nₜ/2*sim.Ω/sim.box.n_periods, sim.box.Nₜ/2*sim.Ω/sim.box.n_periods))
         ylims!((minimum(sim.box.x), maximum(sim.box.x)))
     elseif mode == "lines"
@@ -124,9 +210,12 @@ function plot_ψ̃(sim; mode = "density", x_res=500, ω_res=512, skip = 1, n_lin
         p = plot(x, log.(abs.(ψ̃')), 
                     label = labs,  
                     legend = :outertopright,
+                    tick_direction = :out,
                     linewidth=1.5,
                     xlabel = L"x",
-                    ylabel = L"\log|\tilde{\psi}|")
+                    ylabel = L"\log|\tilde{\psi}|",
+                    margin = 4mm)
+        xlims!((minimum(sim.box.x), maximum(sim.box.x)))
     end
 
     println("Plotting done!")
